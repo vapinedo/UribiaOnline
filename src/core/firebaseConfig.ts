d7bde0568775b68f8fdab680d40b545604e499bc
Victor Pinedo <victorpinedomartinez@gmail.com>
2025-08-05 14:35:56 -0500

    Fix / Update Firebase setup (#1)

--- src/core/firebaseConfig.ts
+++ src/core/firebaseConfig.ts
@@ -3,21 +3,13 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  projectId: 'react-cursos-187dc',
-  messagingSenderId: '791208906607',
-  storageBucket: 'react-cursos-187dc.appspot.com',
-  authDomain: 'react-cursos-187dc.firebaseapp.com',
-  apiKey: 'AIzaSyD1SKumwaNQB8RaZUftyyz1l4duS8nPQPU',
-  appId: '1:791208906607:web:e642a487ddf1b4b4f1a29a',
-
-  apiKey: 'AIzaSyA5esaL0PlvRgv_jDbYWomwMWKisJIsyo4',
-  authDomain: 'portafolioangular-135a6.firebaseapp.com',
-  databaseURL: 'https://portafolioangular-135a6.firebaseio.com',
-  projectId: 'portafolioangular-135a6',
-  storageBucket: 'portafolioangular-135a6.appspot.com',
-  messagingSenderId: '553099220584',
-  appId: '1:553099220584:web:4a71e5d67769bcf3024ddd',
-  measurementId: 'G-1TF14XE18L',
+  apiKey: 'AIzaSyDDqs1hraVJRP3pkY5UAzYVtyCXCqNhYI4',
+  authDomain: 'ng-arriendos-uribia.firebaseapp.com',
+  databaseURL: 'https://ng-arriendos-uribia-default-rtdb.firebaseio.com',
+  projectId: 'ng-arriendos-uribia',
+  storageBucket: 'ng-arriendos-uribia.appspot.com',
+  messagingSenderId: '689295074494',
+  appId: '1:689295074494:web:e0d72794a1862072f72aef',
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
--- src/infrastructure/firebase/firebaseConfig.ts
+++ src/infrastructure/firebase/firebaseConfig.ts
@@ -3,14 +3,13 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  measurementId: 'G-1TF14XE18L',
-  messagingSenderId: '553099220584',
-  projectId: 'portafolioangular-135a6',
-  apiKey: 'AIzaSyA5esaL0PlvRgv_jDbYWomwMWKisJIsyo4',
-  appId: '1:553099220584:web:4a71e5d67769bcf3024ddd',
-  storageBucket: 'portafolioangular-135a6.appspot.com',
-  authDomain: 'portafolioangular-135a6.firebaseapp.com',
-  databaseURL: 'https://portafolioangular-135a6.firebaseio.com',
+  apiKey: 'AIzaSyDDqs1hraVJRP3pkY5UAzYVtyCXCqNhYI4',
+  authDomain: 'ng-arriendos-uribia.firebaseapp.com',
+  databaseURL: 'https://ng-arriendos-uribia-default-rtdb.firebaseio.com',
+  projectId: 'ng-arriendos-uribia',
+  storageBucket: 'ng-arriendos-uribia.appspot.com',
+  messagingSenderId: '689295074494',
+  appId: '1:689295074494:web:e0d72794a1862072f72aef',
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
